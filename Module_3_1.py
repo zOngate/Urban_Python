f15f3d8c1374551b31feea0b4f4fd87943addea1
zOngate <118887641+zOngate@users.noreply.github.com>
2025-01-30 17:41:05 +1000

    Add files via upload

---
+++ Module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.lower()
+    lower_list = []
+    for i in list_to_search:
+        lower_list.append(i.lower())
+    return string in lower_list
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
